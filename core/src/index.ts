6f53da335adadaee39105c44e5f8fe30ed8c0c18
Paulo <pauloeduardo2709@gmail.com>
2019-12-25 01:28:56 -0300

    🎨 Change function and parameter name

--- core/src/index.ts
+++ core/src/index.ts
@@ -4,10 +4,8 @@ import urlRegex from "url-regex";
 /*
   Code based in https://github.com/AmeddahAchraf/Reddit_Video_Download
 */
-export default async function extractMidiaURL(
-  videoURL: string
-): Promise<string> {
-  const response = await fetch(videoURL);
+export default async function extract(shareURL: string): Promise<string> {
+  const response = await fetch(shareURL);
   const body = await response.text();
   const urls = body.match(urlRegex());
   return new Promise<string>(resolve => {
